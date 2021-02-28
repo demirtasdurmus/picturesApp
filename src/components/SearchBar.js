eda571ffb3d74bcc5f0b92113f53ccca9b4f0fb5
Durmuş <demirtasdurmus@users.noreply.github.com>
2021-02-28 23:29:40 +0300

    initial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = { term: "" }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
+        this.setState({term: ""});
+    };
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            placeholder="Write here!"
+                            onChange={e => this.setState({term: e.target.value})}
+                            value={this.state.term}
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+};
+
+
+export default SearchBar;
